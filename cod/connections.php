ce5f4d1803102305d578113fa8232c886f1a0707
BakerPizza <99619436+BakerPizza@users.noreply.github.com>
2022-02-14 20:00:55 +0300

    Update connections.php

--- cod/connections.php
+++ cod/connections.php
@@ -1,6 +1,6 @@
 <?php
 
-$connect = mysqli_connect("bakerpizza.beget.tech", "bakerpizza_forum","Hack1337", "bakerpizza_forum") or die ("error_connect_to_database(idi ti nahuy dalbaeb)". mysqli_error($conn));
+$connect = mysqli_connect("host_name", "username","password", "db_name") or die ("error_connect_to_database(idi ti nahuy dalbaeb)". mysqli_error($conn));
 $connect->set_charset("utf8");
 
 ?>
